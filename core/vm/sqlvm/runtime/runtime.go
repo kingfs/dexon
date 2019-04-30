9f0581707a7f12a730a9ae40f866bc5d501b397e
Meng-Ying Yang <garfield@dexon.org>
2019-04-30 11:42:06 +0800

    core: vm: sqlvm: change OpFunction input type
    The original founction parameters are extracted Instruction attributes.
    To reduce passing parameters, the instruction struct is regard as a
    bundle to carry related parameters for OpFunction.

--- core/vm/sqlvm/runtime/runtime.go
+++ core/vm/sqlvm/runtime/runtime.go
@@ -9,13 +9,18 @@ import (
 // Run is runtime entrypoint.
 func Run(stateDB vm.StateDB, ins []Instruction, registers []*Operand) (ret []byte, err error) {
 	for _, in := range ins {
+		// load register
 		for i := 0; i < len(in.Input); i++ {
 			if !in.Input[i].IsImmediate {
 				in.Input[i] = registers[in.Input[i].RegisterIndex]
 			}
 		}
+		in.Registers = registers
+
+		// jump table and run
 		jUnit := jumpTable[in.Op]
-		errCode := jUnit.Func(&common.Context{}, in.Input, registers, in.Output)
+		in.GasFunc = jUnit.GasFunc
+		errCode := jUnit.Func(&common.Context{}, in)
 		if errCode != nil {
 			err = se.Error{
 				Position: in.Position,
